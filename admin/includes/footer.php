49210481b69589adbfc1b3b53876fcb8ad2047ad
ugesh <ugesh.roopchand@hotmail.com>
2018-04-25 10:48:21 +0400

    WISYWYG editor added to edit_photo.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- WYSIWYG Text editor-->
+     <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+     <script src="js/scripts.js"></script>
+
 </body>
 
 </html>
