e404b21181d85444aaf74acea66e5379c732f83f
ugesh <ugesh.roopchand@hotmail.com>
2018-04-26 09:50:41 +0400

    Dislpayimage  user_image added to users database

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -16,7 +16,7 @@ class User extends Db_object {
 
 
     public function image_path_and_placeholder() {
-        return empty($this->user_image) ? $this->image_placeholder : $this->$upload_directory.DS.$this->user_image;
+        return empty($this->user_image) ? $this->image_placeholder : $this->upload_directory.DS.$this->user_image;
     }
 
 
