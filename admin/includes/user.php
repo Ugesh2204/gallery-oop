e2f234959e83d268b37515649dd77fd161be2c8b
ugesh <ugesh.roopchand@hotmail.com>
2018-04-27 09:26:28 +0400

    Setting the image code in user.php working find

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -14,6 +14,77 @@ class User extends Db_object {
     public $image_placeholder = "http://placehold.it/400x400&text=image";
 
 
+     //This is pass file superglobal $_FILES['upload_file'] as an argument
+     public function set_file($file) {
+
+        if(empty($file) || !$file || !is_array($file)) {
+            $this->errors[] = "There was no file uploaded here";
+            return false;
+    
+            }elseif($file['error'] !=0) {
+    
+            $this->errors[] = $this->upload_errors_array[$file['error']];
+            return false;
+    
+            } else {
+    
+    
+            $this->user_image =  basename($file['name']);
+            $this->tmp_path = $file['tmp_name'];
+            $this->type     = $file['type'];
+            $this->size     = $file['size'];
+    
+    
+            }
+
+    }
+
+
+
+    /*SAVE METHOD TO UPLOAD FILES */
+
+    public function save_user_and_image() {
+
+        /*ERRO checking */
+        if($this->id) {
+            $this->update();
+        } else {
+            if(!empty($this->errors)) {
+                return false;
+            }
+
+            if(empty($this->user_image) || empty($this->tmp_path)) {
+                $this->errors[] = "the file was not available";
+                return false;
+            }
+
+
+            $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->user_image;
+
+            if(file_exists($target_path)) {
+                $this->errors[] = "The file {$this->user_image} already exists";
+                return false;
+            }
+
+            /*move_uploaded_file predifine function */
+            if(move_uploaded_file($this->tmp_path, $target_path)) {
+                if($this->create()) {
+                    unset($this->tmp_path);
+                    return true;
+                }
+            } else {
+                $this->errors[] = "the file directory does not have permission";
+                return false;
+            }
+
+        }
+
+    }
+
+
+
+
+
 
     public function image_path_and_placeholder() {
         return empty($this->user_image) ? $this->image_placeholder : $this->upload_directory.DS.$this->user_image;
