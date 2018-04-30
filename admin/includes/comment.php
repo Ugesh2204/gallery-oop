d1ff6b87765edf8f1212df54f3485f62a22aaf28
ugesh <ugesh.roopchand@hotmail.com>
2018-04-30 10:38:50 +0400

    Self Instantiation Comment Method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -10,9 +10,26 @@ class Comment extends Db_object {
     public $body;
     
 
+    /* Self Instantiation Comment Method*/
 
+    public static function create_comment($photo_id, $author="John", $body=""){
+        if(!empty($photo_id) && !empty($author) && !empty($body)) {
+            
+            $comment = new Comment();
 
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->body = $body;
 
+            return $comment;
+
+        } else {
+
+            return false;
+        }
+    }
+
+}//end comment tag
 
 ?>
 
